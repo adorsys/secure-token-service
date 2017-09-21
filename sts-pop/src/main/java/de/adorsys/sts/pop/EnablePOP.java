5995f9bee1bf194416ed5d0b2bf2fa28e06381be
Boris Skert <boris.skert@adorsys.de>
2017-09-21 13:02:01 +0200

    extract POP controller into separate module

---
+++ sts-pop/src/main/java/de/adorsys/sts/pop/EnablePOP.java
@@ -0,0 +1,17 @@
+package de.adorsys.sts.pop;
+
+import de.adorsys.sts.common.SecureTokenServiceConfig;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target(value = {java.lang.annotation.ElementType.TYPE})
+@Documented
+@Import({
+        PopConfiguration.class
+})
+public @interface EnablePOP {
+}
--- sts-starter/src/main/java/de/adorsys/sts/starter/SecureTokenServiceConfiguration.java
+++ sts-starter/src/main/java/de/adorsys/sts/starter/SecureTokenServiceConfiguration.java
@@ -1,9 +1,11 @@
 package de.adorsys.sts.starter;
 
 import de.adorsys.sts.common.EnableSTS;
+import de.adorsys.sts.pop.EnablePOP;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableSTS
+@EnablePOP
 public class SecureTokenServiceConfiguration {
 }
