70187889c455e0f34343f7225ee95cac9db502cd
Boris Skert <boris.skert@adorsys.de>
2017-09-21 13:14:39 +0200

    extract token-exchange controller to separate module

--- sts-starter/src/main/java/de/adorsys/sts/starter/SecureTokenServiceConfiguration.java
+++ sts-starter/src/main/java/de/adorsys/sts/starter/SecureTokenServiceConfiguration.java
@@ -2,10 +2,12 @@ package de.adorsys.sts.starter;
 
 import de.adorsys.sts.common.EnableSTS;
 import de.adorsys.sts.pop.EnablePOP;
+import de.adorsys.sts.tokenexchange.EnableTokenExchange;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableSTS
 @EnablePOP
+@EnableTokenExchange
 public class SecureTokenServiceConfiguration {
 }
---
+++ sts-token-exchange/src/main/java/de/adorsys/sts/tokenexchange/TokenExchangeConfiguration.java
@@ -0,0 +1,11 @@
+package de.adorsys.sts.tokenexchange;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {
+        "de.adorsys.sts.tokenexchange"
+})
+public class TokenExchangeConfiguration {
+}
