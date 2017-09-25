4f6d2d26098c938c74343af253b563d3b8a25cce
Boris Skert <boris.skert@adorsys.de>
2017-09-25 08:19:13 +0200

    clean up

--- sts-starter/src/main/java/de/adorsys/sts/starter/config/WebSecurityConfig.java
+++ sts-starter/src/main/java/de/adorsys/sts/starter/config/WebSecurityConfig.java
@@ -39,7 +39,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 					"/pop",
 					"/api-docs/**",
 					"/v2/api-docs/**",
-					"/documentation/**",
 					"/swagger-resources/**",
 					"/health",
 					"/health.json",
