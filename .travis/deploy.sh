0426386f2c2dc5cfd0e00f55a62891d932048efe
Boris Skert <boris.skert@adorsys.de>
2017-09-29 11:17:18 +0200

    correct travis config

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,11 @@
-if [[ "$TRAVIS_TAG" =~ ^v[[:digit:].]+$ ]] then
-  echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central"
-  mvn deploy javadoc:javadoc gpg:sign -Prelease -DskipTests -B -U
+#!/usr/bin/env bash
+set -e
+
+TAG_PATTERN="^v([[:digit:]]+\.)+[[:digit:]]+$"
+
+if [[ ${TRAVIS_TAG} =~ ${TAG_PATTERN} ]]; then
+  echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central";
+  mvn deploy javadoc:javadoc gpg:sign -Prelease -DskipTests -B -U;
 else
-  echo "NO RELEASE TAG -> don't publish to mvn central"
-  mvn package -U
+  echo "NO RELEASE TAG -> don't publish to mvn central";
 fi
