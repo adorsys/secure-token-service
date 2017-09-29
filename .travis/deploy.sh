6986e59ba8024d26c73f51d4a3e269027447b97b
Boris Skert <boris.skert@adorsys.de>
2017-09-29 11:28:42 +0200

    temporary set version numbers

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-TAG_PATTERN="^v([[:digit:]]+\.)+[[:digit:]]+$"
+TAG_PATTERN="^v([[:digit:]]+\.)+[[:digit:]]+(-[[:digit:]]+)?$"
 
 if [[ ${TRAVIS_TAG} =~ ${TAG_PATTERN} ]]; then
   echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central";
