ebcf58b57c3d28375553bbf6739a1d07e2718823
Boris Skert <boris.skert@adorsys.de>
2017-10-10 11:24:42 +0200

    introduce ResourceServerManagement annotation activation

--- sts-example/src/main/java/de/adorsys/sts/example/config/ExampleConfiguration.java
+++ sts-example/src/main/java/de/adorsys/sts/example/config/ExampleConfiguration.java
@@ -1,41 +1,19 @@
 package de.adorsys.sts.example.config;
 
+import de.adorsys.sts.resourceserver.EnableResourceServerManagement;
 import de.adorsys.sts.resourceserver.initializer.EnableResourceServerInitialization;
 import de.adorsys.sts.resourceserver.persistence.InMemoryResourceServerRepository;
 import de.adorsys.sts.resourceserver.persistence.ResourceServerRepository;
-import de.adorsys.sts.resourceserver.service.EncryptionService;
-import de.adorsys.sts.resourceserver.service.KeyRetrieverService;
-import de.adorsys.sts.resourceserver.service.ResourceServerService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableResourceServerInitialization
+@EnableResourceServerManagement
 public class ExampleConfiguration {
 
     @Bean
     ResourceServerRepository resourceServerRepository() {
         return new InMemoryResourceServerRepository();
-    }
-
-    @Bean
-    ResourceServerService resourceServerService(
-            ResourceServerRepository repository
-    ) {
-        return new ResourceServerService(repository);
-    }
-
-    @Bean
-    KeyRetrieverService keyRetrieverService(
-            ResourceServerService resourceServerService
-    ) {
-        return new KeyRetrieverService(resourceServerService);
-    }
-
-    @Bean
-    EncryptionService encryptionService(
-            KeyRetrieverService keyRetrieverService
-    ) {
-        return new EncryptionService(keyRetrieverService);
     }
 }
---
+++ sts-resource-server/src/main/java/de/adorsys/sts/resourceserver/ResourceServerManagementConfiguration.java
@@ -0,0 +1,7 @@
+package de.adorsys.sts.resourceserver;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("de.adorsys.sts.resourceserver.service")
+public class ResourceServerManagementConfiguration {
+}
