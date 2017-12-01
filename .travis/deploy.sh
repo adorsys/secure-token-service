e854b8c2aca0da921afb4b1e94e59c8b361ea076
Boris Skert <boris.skert@adorsys.de>
2017-12-01 17:11:44 +0100

    build docker image for sts-example via travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-TAG_PATTERN="^v([[:digit:]]+\.)+[[:digit:]]+(-[[:digit:]]+)?$"
-
-if [[ ${TRAVIS_TAG} =~ ${TAG_PATTERN} ]]; then
-  echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central";
-  mvn --settings .travis/settings.xml package gpg:sign deploy -Prelease -DskipTests -B -U;
-else
-  echo "NO RELEASE TAG -> don't publish to mvn central";
-fi
+/bin/bash .travis/maven_deploy.sh
+/bin/bash .travis/docker_push.sh
