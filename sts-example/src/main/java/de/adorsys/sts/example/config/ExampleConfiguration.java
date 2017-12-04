9c3803e0727a8c0f16d42df44f4e4cd7fcad2f99
Boris Skert <boris.skert@adorsys.de>
2017-12-04 15:46:11 +0100

    sts-example: enable POP endpoint

--- sts-example/src/main/java/de/adorsys/sts/example/config/ExampleConfiguration.java
+++ sts-example/src/main/java/de/adorsys/sts/example/config/ExampleConfiguration.java
@@ -1,6 +1,9 @@
 package de.adorsys.sts.example.config;
 
 import de.adorsys.sts.encryption.EnableEncryption;
+import de.adorsys.sts.keymanagement.persistence.InMemoryKeyStoreRepository;
+import de.adorsys.sts.keymanagement.persistence.KeyStoreRepository;
+import de.adorsys.sts.pop.EnablePOP;
 import de.adorsys.sts.resourceserver.initializer.EnableResourceServerInitialization;
 import de.adorsys.sts.resourceserver.persistence.InMemoryResourceServerRepository;
 import de.adorsys.sts.resourceserver.persistence.ResourceServerRepository;
@@ -10,10 +13,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableResourceServerInitialization
 @EnableEncryption
+@EnablePOP
 public class ExampleConfiguration {
 
     @Bean
     ResourceServerRepository resourceServerRepository() {
         return new InMemoryResourceServerRepository();
+    }
+
+    @Bean
+    KeyStoreRepository keyStoreRepository() {
+        return new InMemoryKeyStoreRepository();
     }
 }
