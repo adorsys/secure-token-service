4c7ae843b83e53b92430e3cbf07d706ba5ef78df
Boris Skert <boris.skert@adorsys.de>
2017-12-04 14:05:32 +0100

    correct docker context path

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -6,7 +6,7 @@ LATEST_IMAGE_NAME="${STS_EXAMPLE_DOCKER_IMAGE_NAME}:latest"
 
 docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME ./sts-example
 docker tag $IMAGE_NAME $LATEST_IMAGE_NAME
 
 docker push $IMAGE_NAME
