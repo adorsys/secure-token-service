ee14b3471db7730d6607abd713f20403e01a34e6
Boris Skert <boris.skert@adorsys.de>
2018-01-30 09:12:42 +0100

    persist keystore and key-entry-attributes into JPA repository

--- sts-spring/src/main/java/de/adorsys/sts/keyrotation/KeyRotationSchedule.java
+++ sts-spring/src/main/java/de/adorsys/sts/keyrotation/KeyRotationSchedule.java
@@ -6,6 +6,7 @@ import de.adorsys.sts.keymanagement.service.KeyRotationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
