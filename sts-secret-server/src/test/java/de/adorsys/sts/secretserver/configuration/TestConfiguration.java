dc35ffd3c2e86b52ae9a82e0dd508bb31592de10
Boris Skert <boris.skert@adorsys.de>
2019-04-16 07:54:09 +0200

    Clean up test code

--- sts-secret-server/src/test/java/de/adorsys/sts/secretserver/configuration/TestConfiguration.java
+++ sts-secret-server/src/test/java/de/adorsys/sts/secretserver/configuration/TestConfiguration.java
@@ -1,57 +1,21 @@
 package de.adorsys.sts.secretserver.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import de.adorsys.lockpersistence.client.LockClient;
-import de.adorsys.lockpersistence.client.NoopLockClient;
 import de.adorsys.sts.decryption.EnableDecryption;
-import de.adorsys.sts.keymanagement.persistence.InMemoryKeyStoreRepository;
-import de.adorsys.sts.keymanagement.persistence.KeyStoreRepository;
-import de.adorsys.sts.resourceserver.persistence.InMemoryResourceServerRepository;
-import de.adorsys.sts.resourceserver.persistence.ResourceServerRepository;
-import de.adorsys.sts.secret.SecretRepository;
 import de.adorsys.sts.secretserver.EnableSecretServer;
-import de.adorsys.sts.secretserver.encryption.InMemorySecretRepository;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableConfigurationProperties
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"de.adorsys.sts.secretserver.helper"})
-@Import(ITSecurityConfiguration.class)
 @EnableSecretServer
 @EnableDecryption
 public class TestConfiguration {
-
-    @Bean
-    public ResourceServerRepository resourceServerRepository() {
-        return new InMemoryResourceServerRepository();
-    }
-
-    @Bean
-    public KeyStoreRepository keyStoreRepository() {
-        return new InMemoryKeyStoreRepository();
-    }
-
-    @Bean
-    public SecretRepository secretRepository() {
-        return new InMemorySecretRepository();
-    }
-
-    @Bean
-    public LockClient lockClient() {
-        return new NoopLockClient();
-    }
-
-    @Bean
-    @Primary
-    @Qualifier("json")
-    public ObjectMapper createJsonObjectMapper() {
-        return new ObjectMapper();
-    }
 
     @Bean
     public RestTemplate restTemplate() {
