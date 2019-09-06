8e7e5e6c1cefec1aa29be89d502016c4155072c6
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-09-06 16:39:29 +0300

    STS-59. Added basic e2e test for key rotation

--- sts-keymanagement/src/main/java/de/adorsys/sts/keymanagement/service/KeyPairGenerator.java
+++ sts-keymanagement/src/main/java/de/adorsys/sts/keymanagement/service/KeyPairGenerator.java
@@ -1,7 +1,6 @@
 package de.adorsys.sts.keymanagement.service;
 
 import de.adorsys.sts.cryptoutils.*;
-import lombok.RequiredArgsConstructor;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.X500NameBuilder;
 import org.bouncycastle.asn1.x500.style.BCStyle;
@@ -12,17 +11,25 @@ import java.security.KeyPair;
 import java.time.Clock;
 import java.util.Date;
 
-@RequiredArgsConstructor
 public class KeyPairGenerator {
 
-    private final static int[] keyUsageSignature = {KeyUsage.nonRepudiation};
-    private final static int[] keyUsageEncryption = {KeyUsage.keyEncipherment, KeyUsage.dataEncipherment, KeyUsage.keyAgreement};
+    private static final int[] keyUsageSignature = {KeyUsage.nonRepudiation};
+    private static final int[] keyUsageEncryption = {KeyUsage.keyEncipherment, KeyUsage.dataEncipherment, KeyUsage.keyAgreement};
 
     private final Clock clock;
     private final String keyAlgo;
     private final Integer keySize;
     private final String serverSigAlgo;
     private final String serverKeyPairName;
+
+    public KeyPairGenerator(Clock clock,
+                            KeyManagementProperties.KeyStoreProperties.KeysProperties.KeyPairProperties keyProperties) {
+        this.clock = clock;
+        this.keyAlgo = keyProperties.getAlgo();
+        this.keySize = keyProperties.getSize();
+        this.serverSigAlgo = keyProperties.getSigAlgo();
+        this.serverKeyPairName = keyProperties.getName();
+    }
 
     public KeyPairEntry generateSignatureKey(String alias, CallbackHandler keyPassHandler) {
         return generate(keyUsageSignature, alias, keyPassHandler);
