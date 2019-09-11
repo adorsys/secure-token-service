e29b9c3640f127f12bd4d2888c74d28257bcc7c9
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-09-11 17:32:03 +0200

    json request and response from files

--- sts-e2e-tests/src/test/java/de/adorsys/sts/tests/e2e/AdminControllerJpaTest.java
+++ sts-e2e-tests/src/test/java/de/adorsys/sts/tests/e2e/AdminControllerJpaTest.java
@@ -1,10 +1,8 @@
 package de.adorsys.sts.tests.e2e;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import de.adorsys.sts.persistence.jpa.config.EnableJpaPersistence;
-import de.adorsys.sts.resourceserver.model.ResourceServer;
 import de.adorsys.sts.tests.BaseEndpointTest;
 import de.adorsys.sts.tests.config.WithAdminConfig;
 import de.adorsys.sts.tests.config.WithoutWebSecurityConfig;
@@ -12,6 +10,8 @@ import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.net.URL;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -25,52 +25,19 @@ class AdminControllerJpaTest extends BaseEndpointTest {
     @Test
     @SneakyThrows
     void getResourseServersTest() {
+        String expectedContent = readFromResource("fixture/admin_controller_response.json");
+
         mvc.perform(get("/admin/resourceServer/")
                 .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
-                .andExpect(content().string(
-                        "{\"servers\":[" +
-                                "{\"endpointUrl\":null," +
-                                "\"userSecretClaimName\":null," +
-                                "\"idpServer\":false," +
-                                "\"jwksUrl\":\"http://localhost:8888/pop\"," +
-                                "\"clientId\":null," +
-                                "\"audience\":\"sts\"" +
-                                "},{" +
-                                "\"endpointUrl\":null," +
-                                "\"userSecretClaimName\":null," +
-                                "\"idpServer\":false," +
-                                "\"jwksUrl\":\"http://localhost:8887/pop\"," +
-                                "\"clientId\":null," +
-                                "\"audience\":\"sts-service-component-example\"" +
-                                "},{" +
-                                "\"endpointUrl\":null," +
-                                "\"userSecretClaimName\":null," +
-                                "\"idpServer\":false," +
-                                "\"jwksUrl\":\"http://sts-service-component:8887/service-component/pop\"," +
-                                "\"clientId\":null," +
-                                "\"audience\":\"sts-service-component\"}" +
-                                "]}"))
+                .andExpect(content().json(expectedContent))
                 .andReturn();
     }
 
     @Test
     @SneakyThrows
     void postResourceServerTest() {
-
-        ResourceServer requestBody = new ResourceServer();
-        requestBody.setAudience("add-example");
-        requestBody.setJwksUrl("http://localhost:8000/test");
-
-        requestBody.setEndpointUrl("http://localhost:8080/multibanking-service");
-        requestBody.setUserSecretClaimName("userSecret");
-        requestBody.setIdpServer(false);
-        requestBody.setClientId("sts-tests");
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-        String requestJson=ow.writeValueAsString(requestBody);
+        String requestJson = readFromResource("fixture/admin_controller_request.json");
 
         mvc.perform(post("/admin/resourceServer/")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -78,5 +45,11 @@ class AdminControllerJpaTest extends BaseEndpointTest {
                 .andExpect(status().is(204))
         .andReturn();
 
+    }
+
+    @SneakyThrows
+    private String readFromResource(String path) {
+        URL url = Resources.getResource(path);
+        return Resources.toString(url, Charsets.UTF_8);
     }
 }
