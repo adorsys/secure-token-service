04cf8ee9778cc59d2ae3336ab9ebf9c2814d4429
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-09-13 17:00:18 +0300

    STS-69. Global dirties context

--- sts-e2e-tests/src/test/java/de/adorsys/sts/tests/BaseSpringTest.java
+++ sts-e2e-tests/src/test/java/de/adorsys/sts/tests/BaseSpringTest.java
@@ -3,11 +3,18 @@ package de.adorsys.sts.tests;
 import de.adorsys.sts.common.tests.BaseMockitoTest;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
+
+/**
+ * Test that assumes we do not do REST calls focusing on pure internals
+ */
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
+@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 public abstract class BaseSpringTest extends BaseMockitoTest {
 }
--- sts-e2e-tests/src/test/java/de/adorsys/sts/tests/KeyRotationContext.java
+++ sts-e2e-tests/src/test/java/de/adorsys/sts/tests/KeyRotationContext.java
@@ -7,13 +7,10 @@ import de.adorsys.sts.tests.config.WithoutWebSecurityConfig;
 import de.adorsys.sts.tests.e2e.testcomponents.PopRotationValidator;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -24,7 +21,6 @@ import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFOR
         WithRotation.class,
         PopRotationValidator.class
 })
-@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface KeyRotationContext {
 }
