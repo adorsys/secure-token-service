fa32f904b8736aabdad597c7b8f72c5b3d9e6c51
Jan-Otto Kröpke <mail@jkroepke.de>
2020-03-26 09:29:55 +0100

    STS-20 Dependency Update (#100)
    * STS-20 Dependency Update
    
    * STS-20 Dependency Update
    
    * STS-20 Dependency Update

--- sts-client-example/src/app/app.module.ts
+++ sts-client-example/src/app/app.module.ts
@@ -4,7 +4,6 @@ import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {KeycloakHttp} from "./keycloak/keycloak.http";
 import {KeycloakService} from "./keycloak/keycloak.service";
-import {HttpModule} from "@angular/http";
 import {StsClientConfig} from "./env/sts-client-config.service";
 import {AppConfig} from "./env/app-config";
 import {HttpClientModule} from "@angular/common/http";
@@ -27,7 +26,6 @@ import {HttpClientModule} from "@angular/common/http";
     KeycloakService,
   ],
   exports: [
-    HttpModule,
     HttpClientModule
   ],
   bootstrap: [AppComponent]
--- sts-client-example/src/app/keycloak/keycloak.http.ts
+++ sts-client-example/src/app/keycloak/keycloak.http.ts
@@ -1,21 +1,19 @@
 import {Injectable} from "@angular/core";
-import {Headers, Http, Request, RequestOptions, RequestOptionsArgs, Response} from "@angular/http";
+import {HttpHeaders, HttpClient, HttpRequest, HttpResponse, HttpEvent} from "@angular/common/http";
 import {KeycloakService} from "./keycloak.service";
 import {Observable} from "rxjs/Rx";
 
 @Injectable()
 export class KeycloakHttp {
-  constructor(private http: Http, private keycloak: KeycloakService) {
+  constructor(private http: HttpClient, private keycloak: KeycloakService) {
   }
 
-  request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
-    let requestOptions = options;
-
+  request(url: string): Observable<any> {
+    const headers = new HttpHeaders();
     if(this.keycloak.hasToken) {
-      let authOptions: RequestOptions = new RequestOptions({headers: new Headers({'Authorization': `Bearer ${this.keycloak.token}`})});
-      requestOptions = new RequestOptions().merge(options).merge(authOptions);
+      headers.append('Authorization', `Bearer ${this.keycloak.token}`)
     }
 
-    return this.http.request(url, requestOptions);
+    return this.http.get(url, { headers });
   }
 }
