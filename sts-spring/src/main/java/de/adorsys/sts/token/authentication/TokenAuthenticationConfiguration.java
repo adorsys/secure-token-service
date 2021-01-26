2a4afe35b5f49ca3530d088e9cfb3e37ab5b364b
Jan-Otto Kröpke <mail@jkroepke.de>
2021-01-26 18:32:28 +0100

    Fixed tests (#109)

--- sts-spring/src/main/java/de/adorsys/sts/token/authentication/TokenAuthenticationConfiguration.java
+++ sts-spring/src/main/java/de/adorsys/sts/token/authentication/TokenAuthenticationConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+import java.time.Clock;
+
 @Configuration
 @ComponentScan(
         basePackages = {"de.adorsys.sts.token.authentication"},
@@ -18,7 +20,7 @@ import org.springframework.context.annotation.FilterType;
 public class TokenAuthenticationConfiguration {
 
     @Bean
-    BearerTokenValidator bearerTokenValidator(AuthServersProvider authServersProvider) {
-        return new LoggingBearerTokenValidator(authServersProvider);
+    BearerTokenValidator bearerTokenValidator(AuthServersProvider authServersProvider, Clock clock) {
+        return new LoggingBearerTokenValidator(authServersProvider, clock);
     }
 }
