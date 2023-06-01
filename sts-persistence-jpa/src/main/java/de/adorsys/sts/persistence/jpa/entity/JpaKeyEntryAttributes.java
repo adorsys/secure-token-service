8d8efb0e70466fd2960cd4d98f8131c886c1eda5
Tim Langohr <>
2023-06-01 15:16:13 +0200

    fix generationStrategy in jpa-entities

--- sts-persistence-jpa/src/main/java/de/adorsys/sts/persistence/jpa/entity/JpaKeyEntryAttributes.java
+++ sts-persistence-jpa/src/main/java/de/adorsys/sts/persistence/jpa/entity/JpaKeyEntryAttributes.java
@@ -18,7 +18,7 @@ import java.time.ZonedDateTime;
 public class JpaKeyEntryAttributes {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long keyStoreId;
--- sts-persistence-jpa/src/main/java/de/adorsys/sts/persistence/jpa/entity/JpaKeyStore.java
+++ sts-persistence-jpa/src/main/java/de/adorsys/sts/persistence/jpa/entity/JpaKeyStore.java
@@ -14,7 +14,7 @@ import java.time.ZonedDateTime;
 public class JpaKeyStore {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
