75843d54c010b0da4a0d6afd4b5dfbfb24618cda
marcelmeyer <marcel.meyer@adorsys.com>
2024-02-06 14:57:09 +0400

    Excluded SecurityAutoConfiguration to prevent Spring from creating a default user

--- sts-secret-server/src/main/java/de/adorsys/sts/secretserver/SecretServerApplication.java
+++ sts-secret-server/src/main/java/de/adorsys/sts/secretserver/SecretServerApplication.java
@@ -1,9 +1,11 @@
 package de.adorsys.sts.secretserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SecretServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecretServerApplication.class, args);
