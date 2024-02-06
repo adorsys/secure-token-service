d2f23dbae0945591fe6c2efb83d97aed0206de9f
marcelmeyer <marcel.meyer@adorsys.com>
2024-02-06 21:32:47 +0400

    Excluded AutoConfiguration for UserDetailsService

--- sts-secret-server/src/main/java/de/adorsys/sts/secretserver/SecretServerApplication.java
+++ sts-secret-server/src/main/java/de/adorsys/sts/secretserver/SecretServerApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class SecretServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecretServerApplication.class, args);
