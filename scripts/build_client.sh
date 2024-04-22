88b33e4e68a292b670431733de8b7890f06b8f60
Maxim Grischenko <maksym.hryshchenko@adorsys.com>
2024-04-22 16:17:51 +0200

    STS-189 update frontend dependencies

--- scripts/build_client.sh
+++ scripts/build_client.sh
@@ -5,10 +5,4 @@ DOCKER_BUILD_PATH=${PWD}/sts-client-example
 
 chmod 777 -R ${DOCKER_BUILD_PATH}
 
-docker run \
-  --rm \
-  -v ${DOCKER_BUILD_PATH}:/opt/src \
-  --entrypoint="" \
-  -w /opt/src \
-  adorsys/ci-build:latest \
-  /opt/src/docker/compile/build.cmd.bash
+docker run --rm -v ${DOCKER_BUILD_PATH}:/opt/src --entrypoint="" -w /opt/src adorsys/ci-build:latest /opt/src/docker/compile/build.cmd.bash
