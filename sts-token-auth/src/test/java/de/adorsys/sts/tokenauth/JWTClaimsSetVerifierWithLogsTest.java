8bbf89493447dedc6da7917425949cc2b910100d
marcelmeyer <marcel.meyer@flendly.de>
2024-09-20 11:00:01 +0200

    Add Mockito dependencies and JWTClaimsSetVerifierWithLogsTest
    This commit adds Mockito dependencies to the pom.xml file to facilitate mocking in unit tests. It also introduces the JWTClaimsSetVerifierWithLogsTest class to test JWT claim set verification, ensuring proper handling of expired and not-before JWT conditions.

---
+++ sts-token-auth/src/test/java/de/adorsys/sts/tokenauth/JWTClaimsSetVerifierWithLogsTest.java
@@ -0,0 +1,57 @@
+package de.adorsys.sts.tokenauth;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.proc.BadJWTException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class JWTClaimsSetVerifierWithLogsTest {
+
+    @org.junit.jupiter.api.Test
+    void verify() {
+        JWTClaimsSetVerifierWithLogs jwtClaimsSetVerifierWithLogs = new JWTClaimsSetVerifierWithLogs(null);
+        assertThrows(NullPointerException.class, () -> jwtClaimsSetVerifierWithLogs.verify(null, null));
+    }
+
+    @Mock
+    private Clock clock;
+
+    private JWTClaimsSetVerifierWithLogs underTest;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        underTest = new JWTClaimsSetVerifierWithLogs(clock);
+    }
+
+    @Test
+    public void testVerify_throwsBadJWTException_whenJWTIsExpired() {
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder().expirationTime(new Date(System.currentTimeMillis() - 60000)).build();
+        when(clock.instant()).thenReturn(Instant.now());
+        assertThrows(BadJWTException.class, () -> {
+            underTest.verify(claimsSet, null);
+        });
+    }
+
+    @Test
+    public void testVerify_throwsBadJWTException_whenJWTIsNotBeforeNow() {
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder().notBeforeTime(new Date(System.currentTimeMillis() + 60000)).build();
+        when(clock.instant()).thenReturn(Instant.now());
+        assertThrows(BadJWTException.class, () -> {
+            underTest.verify(claimsSet, null);
+        });
+    }
+
+}
