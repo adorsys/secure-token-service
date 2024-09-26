6da662adfc39a1b0b717db3075e0b4100c45665b
Maxim Grischenko <maksym.hryshchenko@adorsys.com>
2024-09-26 13:15:23 +0200

    bump up spring boot to 3.3.4 (#215)
    * bump up spring boot to 3.3.4
    
    * remove package-lock.json
    
    * bump up angular to 18.2.6
    
    * build frontend generating package-lock.json
    
    * fix test

--- sts-token-auth/src/test/java/de/adorsys/sts/tokenauth/JWTClaimsSetVerifierWithLogsTest.java
+++ sts-token-auth/src/test/java/de/adorsys/sts/tokenauth/JWTClaimsSetVerifierWithLogsTest.java
@@ -1,44 +1,43 @@
 package de.adorsys.sts.tokenauth;
 
+import com.nimbusds.jose.proc.SecurityContext;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.proc.BadJWTException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Clock;
 import java.time.Instant;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class JWTClaimsSetVerifierWithLogsTest {
 
-    @org.junit.jupiter.api.Test
-    void verify() {
-        JWTClaimsSetVerifierWithLogs jwtClaimsSetVerifierWithLogs = new JWTClaimsSetVerifierWithLogs(null);
-        assertThrows(NullPointerException.class, () -> jwtClaimsSetVerifierWithLogs.verify(null, null));
-    }
-
+    private JWTClaimsSetVerifierWithLogs<SecurityContext> underTest;
     @Mock
     private Clock clock;
 
-    private JWTClaimsSetVerifierWithLogs underTest;
-
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
-        underTest = new JWTClaimsSetVerifierWithLogs(clock);
+        underTest = new JWTClaimsSetVerifierWithLogs<>(clock);
+    }
+
+    @Test
+    void verify() {
+        var jwtClaimsSetVerifierWithLogs = new JWTClaimsSetVerifierWithLogs<>(null);
+        assertThrows(NullPointerException.class, () -> jwtClaimsSetVerifierWithLogs.verify(null, null));
     }
 
     @Test
     public void testVerify_throwsBadJWTException_whenJWTIsExpired() {
-        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder().expirationTime(new Date(System.currentTimeMillis() - 60000)).build();
+        Date exp = new Date(System.currentTimeMillis() - 60000);
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder().expirationTime(exp).build();
         when(clock.instant()).thenReturn(Instant.now());
         assertThrows(BadJWTException.class, () -> {
             underTest.verify(claimsSet, null);
@@ -47,7 +46,8 @@ class JWTClaimsSetVerifierWithLogsTest {
 
     @Test
     public void testVerify_throwsBadJWTException_whenJWTIsNotBeforeNow() {
-        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder().notBeforeTime(new Date(System.currentTimeMillis() + 60000)).build();
+        Date nbf = new Date(System.currentTimeMillis() + 61000);
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder().notBeforeTime(nbf).build();
         when(clock.instant()).thenReturn(Instant.now());
         assertThrows(BadJWTException.class, () -> {
             underTest.verify(claimsSet, null);
